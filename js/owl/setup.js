db0ad341630c5fd92f0c90c1c539d75bc3b18463
Jefferzom <jeffsilvati@hotmail.com>
2021-02-16 18:37:35 -0300

    devflix

---
+++ js/owl/setup.js
@@ -0,0 +1,17 @@
+// Carrosel
+$('.owl-carousel').owlCarousel({
+  loop: false,
+  margin: 10,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
